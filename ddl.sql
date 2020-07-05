f3b1d3d4b0e948eb4a546b946d6c41e5755a7a49
张伟鑫 <772768619@qq.com>
2020-07-05 20:44:56 +0800

    ddl

--- ddl.sql
+++ ddl.sql
@@ -10,5 +10,11 @@ CREATE TABLE
         input_Date VARCHAR(16),
         user_type VARCHAR(10),
         PRIMARY KEY (user_id)
-    )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_c
+    );
+   -- ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_c
+create table menu_info(
+menu_id VARCHAR(16) not null,
+menu_name VARCHAR(32) not null,
+menu_level VARCHAR(4) not null,
+PRIMARY KEY(menu_id)
+);
